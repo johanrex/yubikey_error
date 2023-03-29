ea6ed7f91283c1090783f7b2201654bf8bcd0597
Johan Rex <serexj@tetrapak.com>
2023-03-29 12:37:56 +0200

    ...

---
+++ testclient_published/Program.cs
@@ -0,0 +1,6 @@
+﻿using testlib;
+
+Console.WriteLine("Hello, World!");
+var c = new testlib.Class1();
+var count = c.CountDevices();
+Console.WriteLine($"Found {count} devices.");
---
+++ testlib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Yubico.YubiKey;
+
+namespace testlib
+{
+    public class Class1
+    {
+        public int CountDevices()
+        {
+            var devices = YubiKeyDevice.FindAll();
+            return devices.Count();
+        }
+    }
+}
